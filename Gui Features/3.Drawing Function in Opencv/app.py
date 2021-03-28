f0488e64fca80f2f96bb2d24e27ef48b7cc406b0
muhammad-asher <kmuhammadasher0308@gmail.com>
2021-03-28 22:20:58 +0500

    Update app.py

--- Gui Features/3.Drawing Function in Opencv/app.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-#Load Image
-img = cv2.imread("./Images-Videos/sun.png")
-# with 0 => gray scale image
-#Display Image
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
