227895e6c0f60c95e83048ddef4ba18b0c0255ad
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-26 09:00:14 +0500

    Create createBorder.py

---
+++ Gui Features/createBorder.py
@@ -0,0 +1,19 @@
+# Python program to explain cv2.copyMakeBorder() method
+
+# importing cv2
+import cv2
+
+# path
+path = r'F:\SE-Courses\Projects\Python\Opencv-Py-Programs\Gui Features\sun.png'
+
+# Reading an image in default mode
+image = cv2.imread(path)
+
+# Window name in which image is displayed
+window_name = 'Image'
+
+# Using cv2.copyMakeBorder() method
+image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT)
+
+# Displaying the image
+cv2.imshow(window_name, image)
